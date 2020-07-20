96bfd37ecb682e41173ff2223e06192c0da3f952
ethan4335 <ethanfresnel@gmail.com>
2020-07-20 14:21:11 +0800

    add examples and homework

---
+++ classify/homework_1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = 'my_pytorch_note'
+__author__ = 'deagle'
+__date__ = '7/20/2020 14:03'
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓     ┏┓
+            ┏━┛┻━━━━━┛┻━┓
+            ┃     ☃     ┃
+            ┃  ┳┛   ┗┳  ┃
+            ┃     ┻     ┃
+            ┗━┓       ┏━┛
+              ┃       ┗━━━━┓
+              ┃   神兽保佑  ┣┓
+              ┃   永无BUG！ ┣┛
+              ┗━┓┓┏━━━━┳┓┏━┛
+                ┃┫┫    ┃┫┫
+                ┗┻┛    ┗┻┛
+"""
+
+if __name__ == '__main__':
+    print('this message is from main function')
